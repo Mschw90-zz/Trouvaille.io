0a6a8d79526b7546c0aaf4d0313191b07b8e9612
Alex <aglaze132@Gmail.com>
2017-12-08 10:44:11 -0800

    pulled matts udpate and inserted the newTripUpdate

---
+++ components/NewDriveScreen.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View, Image, TouchableOpacity, TextInput, Alert, AsyncStorage } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import {
+  Container, Header, Button, Body, Icon, Content, Item, Form, Input } from 'native-base';
+
+export default class NewDriveScreen extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  DriverDestinationMap() {
+    this.props.navigation.navigate('DriverMap');
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Button onPress={() => {this.DriverDestinationMap()}} style={{paddingTop: 88, marginBottom: 33, width: Dimensions.get('window').width}}><Text>Da Map</Text></Button>
+        </Header>
+        <Content>
+          <Form>
+            <Item>
+              <Input placeholder="Date of Trip" />
+            </Item>
+            <Item last>
+              <Input placeholder="Destination" />
+            </Item>
+            <Item last>
+              <Input placeholder="Why are you travelling?" />
+            </Item>
+            <Item last>
+              <Input placeholder="# of Seats" />
+            </Item>
+            <Item last>
+              <Input placeholder="What are you driving? Include your make
+                model and year." />
+            </Item>
+            <Item last>
+              <Input placeholder="Cost of trip" />
+            </Item>
+            <Item last>
+              <Input placeholder="$ per contributor" />
+            </Item>
+            <Item last>
+              <Input placeholder="$ for front seat" />
+            </Item>
+            <Item last>
+              <Input placeholder="Discussion - Use this to chat and figure out
+                what you need to have a fun trip!" />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
+    )
+  }
+}
--- components/NewTripScreen.js
+++ components/NewTripScreen.js
@@ -26,16 +26,45 @@ import styles from '../styles.js'
 
 
 export default class NewTripScreen extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {}
-  }
+    constructor(props){
+      super(props);
+      this.state = {}
+    }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>You made it to the new trip page!</Text>
-      </View>
-    )
+    newDrivePage() {
+      this.props.navigation.navigate('NewDrive');
+    }
+
+    newPassengerPage() {
+      this.props.navigation.navigate('NewPassenger');
+    }
+
+    render() {
+      return (
+        <Container>
+
+
+          <Content>
+            <Text style={{textAlign:'center', fontWeight:'bold', fontSize: 35}}>
+              Would you like to be a driver or passenger?
+            </Text>
+            <View style={{flexWrap: 'wrap', alignItems: 'flex-start', flexDirection: 'row', marginTop: 115, marginLeft: 33}}>
+              <Button style={{backgroundColor: '#379df1', width: 150, height: 150, paddingTop: 55, paddingLeft: 20}}>
+                <Icon name='ios-car' onPress={() => {this.newDrivePage()}} style={{fontSize: 90, marginBottom: 50, color: '#f7d432'}} />
+              </Button>
+              <Button transparent style={{marginLeft: 10, backgroundColor: '#379df1', width: 150, height: 150, paddingTop: 15, paddingLeft: 20}}>
+                <Icon name='ios-briefcase' onPress={() => {this.newPassengerPage()}} style={{color: '#f7d432', fontSize: 90}}/>
+              </Button>
+            </View>
+          </Content>
+          <Footer>
+            <FooterTab>
+              <Button full>
+                <Text>Footer</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
+      )
+    }
   }
-}
