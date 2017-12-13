acdf5287cc205659f05452b0dfd3d815dba8e0a1
Alex <aglaze132@Gmail.com>
2017-12-13 15:46:41 -0800

    finsihed specific trip mockup

--- components/SpecificTripScreen.js
+++ components/SpecificTripScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Text} from 'react-native'
+import {View, Text} from 'react-native';
+import {Icon} from 'native-base';
 
 export default class SpecificTripScreen extends React.Component {
   constructor(props) {
@@ -10,10 +11,19 @@ export default class SpecificTripScreen extends React.Component {
   render() {
     return (
       <View>
-        <Text>Mapped Destination</Text>
-        <Text>Date, 01, 2017</Text>
-        <Text>Name of Trip (Coachella Roadtrip)</Text>
-        <Text>(100) seats left</Text>
+        <View style={{alignItems: 'center'}}>
+          <Text>Mapped Destination</Text>
+        </View>
+        <View style={{alignItems: 'center'}}>
+          <Icon name='ios-happy' />
+          <Text>Date, 01, 2017</Text>
+          <Text>Alex Glaze is going to: Name of Trip (Coachella Roadtrip)</Text>
+          <Text>(100) seats left</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Icon style={{color: 'blue'}} name='ios-heart' /><Text style={{color: 'red', alignSelf: 'flex-end'}}>0</Text>
+            <Icon style={{color: 'blue'}} name='ios-chatbubbles' /><Text style={{color: 'red', alignSelf: 'flex-end'}}>0</Text>
+          </View>
+        </View>
       </View>
     )
   }
