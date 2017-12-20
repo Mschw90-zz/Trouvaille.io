9f99fd94e3f19f406828a56d171f207eda048350
Alex <aglaze132@Gmail.com>
2017-12-20 11:02:32 -0800

    chat is hard coded for demo

--- bunkData/messages.js
+++ bunkData/messages.js
@@ -1,11 +1,11 @@
 module.exports = [
   {
     _id: Math.round(Math.random() * 1000000),
-    text: 'Yes, and I use Gifted Chat!',
-    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+    text: 'Roger that!',
+    createdAt: new Date(Date.UTC(2017, 11, 21, 17, 25, 0)),
     user: {
       _id: 1,
-      name: 'Developer',
+      name: 'Alex Glaze',
     },
     sent: true,
     received: true,
@@ -16,17 +16,17 @@ module.exports = [
   },
   {
     _id: Math.round(Math.random() * 1000000),
-    text: 'Are you building a chat app?',
-    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+    text: 'Can anyone bring an extra phone charger?',
+    createdAt: new Date(Date.UTC(2017, 11, 21, 17, 20, 0)),
     user: {
       _id: 2,
-      name: 'React Native',
+      name: 'Matt Schwartz',
     },
   },
   {
     _id: Math.round(Math.random() * 1000000),
-    text: "You are officially rocking GiftedChat.",
-    createdAt: new Date(),
+    text: "Your trip starts today at 5PM.",
+    createdAt: new Date(Date.UTC(2017, 11, 21, 17, 25, 0)),
     system: true,
   },
 ];
--- bunkData/old_messages.js
+++ bunkData/old_messages.js
@@ -1,27 +1,53 @@
 module.exports = [
   {
     _id: Math.round(Math.random() * 1000000),
+    text: "Tyrone Scafe changed the trip time to 5PM",
+    createdAt: new Date(Date.UTC(2017, 11, 10, 17, 20, 0)),
+    system: true
+  },
+  {
+    _id: Math.round(Math.random() * 1000000),
     text:
-      "It uses the same design as React, letting you compose a rich mobile UI from declarative components https://facebook.github.io/react-native/",
-    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+      "Sounds good",
+    createdAt: new Date(Date.UTC(2017, 11, 10, 17, 28, 0)),
     user: {
-      _id: 1,
-      name: "Developer"
+      _id: 2,
+      name: "Tyrone Scafe"
     }
   },
   {
     _id: Math.round(Math.random() * 1000000),
-    text: "React Native lets you build mobile apps using only JavaScript",
-    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+    text:
+      "That works for me",
+    createdAt: new Date(Date.UTC(2017, 11, 10, 17, 27, 0)),
     user: {
       _id: 1,
-      name: "Developer"
+      name: "Alex Glaze"
     }
   },
   {
     _id: Math.round(Math.random() * 1000000),
-    text: "This is a system message.",
-    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+    text:
+      "Let's do 5PM",
+    createdAt: new Date(Date.UTC(2017, 11, 10, 17, 25, 0)),
+    user: {
+      _id: 2,
+      name: "Matt Schwartz"
+    }
+  },
+  {
+    _id: Math.round(Math.random() * 1000000),
+    text: "What time would you guys wanna head out?",
+    createdAt: new Date(Date.UTC(2017, 11, 10, 17, 20, 0)),
+    user: {
+      _id: 2,
+      name: "Tyrone Scafe"
+    }
+  },
+  {
+    _id: Math.round(Math.random() * 1000000),
+    text: "Tyrone created this trip for 12-21-2017",
+    createdAt: new Date(Date.UTC(2017, 11, 10, 17, 20, 0)),
     system: true
   }
 ];;
